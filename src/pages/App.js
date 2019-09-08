9b2a838303de90ac7dea760ad51c9c6fa7165f8c
vergil465 <martinluik@hotmail.com>
2019-09-08 14:07:14 +0300

    updated dependencies

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import '../resources/scss/base.scss';
+import { Layout } from 'react-simple-responsive-framework';
 
 export default class App extends Component {
   render() {
-    return this.props.children;
+    return (
+      <Layout>
+        {this.props.children}
+      </Layout>
+    );
   }
 }
